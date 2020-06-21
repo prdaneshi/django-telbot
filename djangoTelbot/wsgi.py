3e0db3a2f02fa28d1f4fffc66b4655660464c2d2
amin-da75 <m.amin.daneshi96@gmail.com>
2020-06-21 15:45:14 +0430

    host setting

--- djangoTelbot/wsgi.py
+++ djangoTelbot/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoTelbot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoTelbot.settings.production')
 
 application = get_wsgi_application()
